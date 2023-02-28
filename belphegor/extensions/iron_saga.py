add582f22ea5b0beb7ef86d471b80082494c90bb
nguuuquaaa <nguuuquaaa@gmail.com>
2023-03-01 01:41:07 +0700

    remove default description in template

--- belphegor/extensions/iron_saga.py
+++ belphegor/extensions/iron_saga.py
@@ -299,6 +299,7 @@ class PilotDisplay(paginators.BasePaginator):
 #=============================================================================================================================#
 
 class SkillEmbedTemplate(paginators.PaginatorEmbedTemplate):
+    description = None
     fields: Callable = lambda item, index: (
         item.value.en_name,
         f"**{item.value.skills[0].name} {'[' + item.value.skills[0].copilot + ']' if item.value.skills[0].copilot else ''}**\n{item.value.skills[0].effect}",
