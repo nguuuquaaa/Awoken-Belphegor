6c86ac33175b96ff064d63df2d23f102bfc5a0dc
nguuuquaaa <nguuuquaaa@gmail.com>
2023-08-12 12:23:11 +0700

    use pydantic-settings

--- belphegor/settings.py
+++ belphegor/settings.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings, validator
+from pydantic_settings import BaseSettings
 
 #=============================================================================================================================#
 
